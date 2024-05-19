649a1b58d114339ab7380d7c98f78bced7824f33
Mehrad Jalali <107107924+mehradjalali@users.noreply.github.com>
2024-05-19 18:23:53 +0330

    Revert "Auth updated by laravel for two step verification"
    This reverts commit 8f754b77465e44c1ccf387101841b4d67c553ba8.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements MustVerifyEmail {
+class User extends Authenticatable
+{
     use HasFactory, Notifiable;
 
     /**
@@ -36,7 +37,8 @@ class User extends Authenticatable implements MustVerifyEmail {
      *
      * @return array<string, string>
      */
-    protected function casts(): array {
+    protected function casts(): array
+    {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,140 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="en">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <!-- This file has been downloaded from Bootsnipp.com. Enjoy! -->
+    <title>Admin Panel</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{asset('assets/styles/style.css')}}" rel="stylesheet">
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<style>
+.bs-example {
+    margin: 20px;
+}
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+.modal-content iframe {
+    margin: 0 auto;
+    height: 300px;
+    width: 300px;
+    display: block;
+}
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+.center {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 10px;
+}
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+.navbar-default .navbar-nav>li>a:hover,
+.navbar-default .navbar-nav>li>a:focus {
+    background-color: #FFFF00;
+    color: #FFC0CB;
+}
+</style>
+<script>
+$(document).ready(function() {
+    /* Get iframe src attribute value i.e. YouTube video url
+    and store it in a variable */
+    var url = $("#cartoonVideo").attr('src');
+
+    /* Assign empty url value to the iframe src attribute when
+    modal hide, which stop the video playing */
+    $("#myModal").on('hide.bs.modal', function() {
+        $("#cartoonVideo").attr('src', '');
+    });
+
+    /* Assign the initially stored url back to the iframe src
+    attribute when modal is displayed again */
+    $("#myModal").on('show.bs.modal', function() {
+        $("#cartoonVideo").attr('src', url);
+    });
+});
+</script>
+
+<Body background="{{asset('assets/img/bg.jpg')}}">
+
+    <body>
+        <div id="wrapper">
+            <nav class="navbar header-top fixed-top navbar-expand-lg  navbar-dark bg-dark">
+                <div class="container">
+                    <img src="{{asset('assets/img/logo.png')}}" width=" 35" height="35"></img>
+                    <button class=" navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
+                        aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarText">
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            @if (Auth::check())
+                            <ul class="navbar-nav side-nav">
+                                <li class="nav-item">
+                                    <a href="{{route('home')}}" class="nav-link text-white"
+                                        style="margin-left: 20px;">Home
+                                        <span class="sr-only">(current)</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{route('videos.all')}}"
+                                        style="margin-left: 20px;">Videos</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{route('video.upload')}}"
+                                        style="margin-left: 20px;">Upload</a>
+                                </li>
+
+                            </ul>
+                            @endif
+                            <ul class="navbar-nav ml-md-auto d-md-flex float-right">
+                                @if (Auth::check())
+                                <li class="nav-item">
+                                    <a class="nav-link" id="navbarDropdown" role="button" data-toggle="dropdown"
+                                        aria-haspopup="true" aria-expanded="false">
+                                        {{ Auth::user()->name }}
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </li>
+                                @else
+                                <li class="nav-item">
+                                    <a href="{{ route('register') }}" class="nav-link">Register</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ route('login') }}" class="nav-link">Login</a>
+                                </li>
+                                @endif
+                            </ul>
+                        </div>
+
+                        </ul>
                     </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                </div>
+            </nav>
+            <div class="container-fluid">
+                <main class="py-4">
+                    @yield('content')
+                </main>
+            </div>
         </div>
+        <script type="text/javascript">
+
+        </script>
     </body>
+
 </html>
