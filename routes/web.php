8f754b77465e44c1ccf387101841b4d67c553ba8
Mehrad Jalali <107107924+mehradjalali@users.noreply.github.com>
2024-05-19 18:20:50 +0330

    Auth updated by laravel for two step verification

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return Redirect::route('home');
+    return view('welcome');
 });
 
-Auth::routes();
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/all-videos/', [App\Http\Controllers\Video\VideoController::class, 'allVideos'])->name('videos.all')->middleware('auth:web');
-Route::get('/upload-video', [App\Http\Controllers\Video\VideoController::class, 'uploadVideo'])->name('video.upload');
-Route::post('/upload-video', [App\Http\Controllers\Video\VideoController::class, 'storeVideo'])->name('video.store');
-Route::get('/delete-video/{id}', [App\Http\Controllers\Video\VideoController::class, 'deleteVideo'])->name('video.delete');
-Route::get('/edit-video/{id}', [App\Http\Controllers\Video\VideoController::class, 'videoDetails'])->name('video.details');
-Route::post('/edit-video/{id}', [App\Http\Controllers\Video\VideoController::class, 'editVideo'])->name('video.update');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
